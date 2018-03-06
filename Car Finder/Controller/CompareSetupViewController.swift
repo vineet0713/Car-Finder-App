01582af3afe2dd049d67f5a62fc1076a38f8f3c2
Vineet Joshi <vineet0713@gmail.com>
2018-03-05 17:05:46 -0800

    Setup a basic "sketch" of what the UI would look like (on Main.storyboard).

---
+++ Car Finder/Controller/CompareSetupViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CompareSetupViewController.swift
+//  Car Finder
+//
+//  Created by Vineet Joshi on 3/5/18.
+//  Copyright © 2018 Vineet Joshi. All rights reserved.
+//
+
+import UIKit
+
+class CompareSetupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
